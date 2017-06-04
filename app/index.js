8e30cd3f77f80c32e12d04ae4b4fc83644a641f5
isAndy99 <claudiu.at@gmail.com>
2017-06-04 16:19:43 +0300

    Create basic 'Hello World' React app

--- app/index.js
+++ app/index.js
@@ -1,3 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
+
+// Component's properties:
+// state
+// lifecycle event
+// UI -> required
+
+// Component definition
+class App extends React.Component { 
+    render() {
+        return (
+            <div>
+                Hello React Training!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
