9e8a0d38b51461f1138e575c56ea40ed0802a71f
isAndy99 <claudiu.at@gmail.com>
2017-06-08 19:54:07 +0300

    Implement Loading component

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,11 +4,12 @@ var queryString = require('query-string');
 var api = require('../utils/api');
 var Link = require('react-router-dom').Link;
 var PlayerPreview = require('./PlayerPreview');
+var Loading = require('./Loading');
 
 
 function Profile(props) {
   var info = props.info;
-  
+
   return (
     <PlayerPreview avatar={info.avatar_url} username={info.login}>
       <ul className='space-list-items'>
@@ -81,7 +82,7 @@ class Results extends React.Component {
     var loading = this.state.loading;
 
     if (loading === true) {
-      return <p>Loading</p>
+      return <Loading />
     }
     if (error) {
       return (
